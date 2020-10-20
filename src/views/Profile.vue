2bd10376dc20c5a4aaeb7ff68e74ae109b15c762
Leah Roberts <onalani10@gmail.com>
2020-10-19 22:01:48 -0600

    worked on create account page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,15 +1,165 @@
 <template>
-    <h1>Create a Photographer's Account</h1> <!--can use veutify to make text entry components-->
+    <meta name='viewport'
+      content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+    <h1>Create a Photographer's Account</h1>
+
+    <div id="profile">
+        <div class="img-div">
+            <div id="img-cont" @click="addPhoto">
+                <i class='far fa-user-circle'></i>
+                <h2>Upload Photo</h2>
+            </div>
+        </div>
+        <div class="prof">
+            <h3>Personal</h3>
+            <form>
+                <input type="text" v-model="fName" placeholder="First Name" required>
+                <input type="text" v-model="lName" placeholder="Last Name" required>
+                <input type="email" v-model="email" placeholder="Email" required>
+                <input type="password" v-model="pwd" placeholder="Password" required>
+                <textarea id="bio" type="text" v-model="bio" placeholder="Profile Biography" maxlength="500" required/>
+            </form>
+            <p>Characters left:</p> <!-- {{ charactersLeft }}</p>-->
+            <h3>Location</h3>
+            <form>
+                <input class="loc-text" type="text" v-model="country" placeholder="Country" required>
+                <input class="loc-text" type="text" v-model="state" placeholder="State" required>
+                <input class="loc-text" type="text" v-model="city" placeholder="City" required>
+                <input id="spec" v-model="specialty" placeholder="Specialty" required>
+            </form>
+        </div>
+    </div>
+
+    <button><h2>Submit</h2></button>
 </template>
 
 <script>
-export default {
 
+export default {
+    data() {
+        return {
+            fName: "",
+            lName: "",
+            email: "",
+            pwd: "",
+            bio: "",
+            country: "",
+            state: "",
+            city: "",
+            specialty: "",
+        }
+    },
+    mounted() {
+      let recaptchaScript = document.createElement('script');
+      recaptchaScript.setAttribute('src', 'https://kit.fontawesome.com/a076d05399.js');
+      recaptchaScript.async = true;
+      document.head.appendChild(recaptchaScript);
+    },
+    methods: {
+      addPhoto() {
+          alert("worked!");
+      },
+  }
 }
 </script>
 
 <style scoped>
 
+i {
+    align-self: center;
+}
 
+input {
+    border-radius: 5px;
+    border: 1px solid;
+}
 
+#bio {
+    border: 1px solid;
+    border-radius: 5px;
+    resize: none;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+button {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background-color: #B9CFF7;
+    text-align: center;
+    border-radius: 5px;
+    border-style: none;
+}
+
+button:hover {
+    background-color: #9BBBF4;
+}
+
+@media only screen and (min-width: 961px) {
+    #profile {
+        padding: 0px 50px 50px 50px;
+        display: flex;
+        flex-direction: row-reverse;
+        justify-content: space-between;
+    }
+
+    .prof {
+        flex: 75%;
+    }
+
+    .img-div {
+        margin-right: 50px;
+        flex: 25%;
+    }
+    
+    .img-div:hover {
+        cursor: pointer;
+    }
+    i {
+        margin-top: 30px;
+        font-size: 300px;
+        align-self: center;
+    }
+
+    p {
+        text-align: left;
+    }
+
+    h3 {
+        text-align: left;
+    }
+
+    form {
+        display: flex;
+        justify-content: flex-start;
+        flex-wrap: wrap;
+    }
+
+    input {
+        width: 317px;
+        height: auto;
+        padding: 10px;
+        margin-bottom: 10px;
+        margin-right: 10px;
+    }
+
+    .loc-text {
+        width: 200px;
+    }
+
+    #spec {
+        width: 665px;
+    }
+
+    #bio {
+        padding: 10px;
+        width: 681px;
+        height: 100px;
+    }
+
+    button {
+        margin-bottom: 50px;
+        width: 400px;
+        height: auto;
+    }
+}
 </style>
