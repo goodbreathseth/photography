7c8b42d3252661c8c263e529693f3fdb339bbf01
Parker Wilson <parker@tract.us>
2020-10-20 12:11:42 -0600

    Started implementing photographers with dummy data on the home page.

---
+++ src/components/Photographers.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="photographers">
+    
+    <div v-bind:key="photographer.id" v-for="photographer in photographers">
+        <h2> {{photographer.name}} </h2>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Photographers",
+    props: ["photographers"]
+    
+}
+</script>
+
+<style scoped>
+
+    .photographers {
+        background-color: rgb(182, 176, 176);
+        text-align: left;
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,50 @@
 <div class="homepage">
     <h1>Connect Me</h1>
     <h3>Find a Photographer Now!</h3>
+    <Photographers v-bind:photographers="photographers"/>
 </div>
 </template>
 
 <script>
+import Photographers from '@/components/Photographers.vue'
+
+
 export default {
-    
+    components: {
+        Photographers
+    },
+    data() {
+        return {
+            photographers: [
+                {
+                    id: 1,
+                    name: "Andrew Jones",
+                    location: "Salt Lake City, Utah",
+                    bio: "...",
+                    email: "fake1@gmail.com",
+                    specialty: "Weddings"
+                },
+                {
+                    id: 2,
+                    name: "Sarah Hemsworth",
+                    location: "Farmington, Utah",
+                    bio: "...",
+                    email: "fake2@gmail.com",
+                    specialty: "Family"
+                },
+                {
+                    id: 3,
+                    name: "Josh Holland",
+                    location: "San Clamente, California",
+                    bio: "...",
+                    email: "fake3@gmail.com",
+                    specialty: "Weddings"
+                },
+            ]
+        }
+
+    }
+
 }
 </script>
 
